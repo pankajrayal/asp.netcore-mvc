d4671668c33002d2e63d95aa597d74e76e5d0b5d
Pankaj Rayal <https://github.dxc.com/>
2021-06-15 21:23:35 +0530

    Added Model and DBContext

---
+++ BookListRazor/BookListRazor/Models/BookDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Models
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options) { }
+        public DbSet<Book> Book { get; set; }
+    }
+
+}
