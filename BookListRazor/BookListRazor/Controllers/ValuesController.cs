620518b23212583890bd1868e894ca24f81a038e
Pankaj Rayal <https://github.dxc.com/>
2021-06-17 09:25:03 +0530

    Setting up the BlukyBook project

---
+++ BookListRazor/BookListRazor/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Values : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var employee = new   { id = 1, name = "Pankaj Rayal" };
+            return Ok(employee);
+        }
+    }
+}
